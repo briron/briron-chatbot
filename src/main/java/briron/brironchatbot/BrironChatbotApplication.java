1dc41ea08d8a27b7f45d7188b0db0942dc1ad4d5
kim <youoper@gmail.com>
2020-08-22 20:18:51 +0900

    make a new spring boot project

---
+++ src/main/java/briron/brironchatbot/BrironChatbotApplication.java
@@ -0,0 +1,13 @@
+package briron.brironchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrironChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrironChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/briron/brironchatbot/BrironChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package briron.brironchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrironChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
